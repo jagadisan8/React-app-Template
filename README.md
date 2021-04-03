d3f9e86d525f86e454dfd3706187f7692fd80b4e
Jagadisan Saravanan <sjagadish717@gmail.com>
2021-04-04 02:30:08 +0530

    React-App-Template

---
+++ README.md
@@ -0,0 +1,3 @@
+# React App Template
+
+REACT APP TEMPLATE SOURCE CODE
---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  
+          
+    render(){
+    
+        return(
+            <div>
+            <h1>React App Template</h1> 
+            </div>
+
+            
+
+        )
+    
+    }
+
+}
+
+export default App;
